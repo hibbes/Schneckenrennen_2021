3c2959f608384fe51abb724761ad81df4c9fd575
hibbes <marek@czernohous.de>
2026-03-31 22:40:22 +0200

    Kommentare: Programmfluss erklärt

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,32 @@
-
+/**
+ * Startet eine Rennsimulation mit zwei Schnecken und gibt den Sieger aus.
+ *
+ * <p>Demonstriert das Zusammenspiel der Klassen {@link Rennschnecke},
+ * {@link Rennen} und die Verwendung von {@link Rennen#durchfuehren()} mit
+ * automatischer Schleife bis zum Ziel.</p>
+ *
+ * @author hibbes
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		Rennschnecke Alfred = new Rennschnecke("Alfred", "Weinschnecke", 2.1);
-		Rennschnecke Barbara = new Rennschnecke("Barbara", "Seeschnecke", 2.1);
-		Rennen Schillerrace = new Rennen("Schillerrace", 7);
-		Schillerrace.addRennschnecke(Alfred);
-		Schillerrace.addRennschnecke(Barbara);
-		Schillerrace.durchfuehren();
-		System.out.println(Schillerrace.ermittleGewinner());
-		
-	
+    /**
+     * @param args Kommandozeilenargumente (nicht verwendet)
+     */
+    public static void main(String[] args) {
 
-	
-	}
-	
-	
+        // Zwei Schnecken mit gleicher Höchstgeschwindigkeit → fairer Wettbewerb
+        Rennschnecke Alfred  = new Rennschnecke("Alfred",  "Weinschnecke", 2.1);
+        Rennschnecke Barbara = new Rennschnecke("Barbara", "Seeschnecke",  2.1);
+
+        // Rennen "Schillerrace" mit Streckenlänge 7 Einheiten
+        Rennen Schillerrace = new Rennen("Schillerrace", 7);
+        Schillerrace.addRennschnecke(Alfred);
+        Schillerrace.addRennschnecke(Barbara);
+
+        // Rennen starten: läuft automatisch bis eine Schnecke die Ziellinie erreicht
+        Schillerrace.durchfuehren();
+
+        // Sieger ausgeben
+        System.out.println("Sieger: " + Schillerrace.ermittleGewinner());
+    }
 }
